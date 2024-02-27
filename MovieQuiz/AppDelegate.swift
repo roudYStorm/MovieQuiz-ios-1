e71e7b6835e8c99836d82c83943706c9b33b1f42
roudYStorm <anzhelikaroud@yandex.ru>
2024-02-27 17:29:18 +0300

    для проверки 2

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -1,11 +1,12 @@
 import UIKit
 
 @main
-final class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegater: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        
         return true
     }
 
